b660798e7041ba1b2335652bbf918103dd582875
Leandro <leabru29@gmail.com>
2025-07-17 09:53:23 -0300

    rotas para o CRUD de api de cupons

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\CupomController;
 use App\Http\Controllers\ProdutoController;
 use Illuminate\Support\Facades\Route;
 
 Route::apiResource('clientes', ClienteController::class);
 Route::apiResource('produtos', ProdutoController::class);
+Route::apiResource('cupons', CupomController::class);
