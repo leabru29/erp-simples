29a1eab176fd8bdf4987bda49f530f2a32688584
Leandro <leabru29@gmail.com>
2025-07-18 08:49:01 -0300

    Rotas para api de carrinho

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CarrinhoController;
 use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\CupomController;
 use App\Http\Controllers\EstoqueController;
@@ -14,3 +15,6 @@ Route::apiResource('cupons', CupomController::class);
 Route::apiResource('variacoes', VariacaoController::class);
 Route::apiResource('estoques', EstoqueController::class);
 Route::apiResource('pedidos', PedidoController::class);
+
+Route::post('/carrinho/adicionar', [CarrinhoController::class, 'adicionarProdutoAoCarrinho']);
+Route::post('/carrinho/finalizar', [CarrinhoController::class, 'finalizar']);
